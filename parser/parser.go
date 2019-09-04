8374bf2d207de9d6009f8af1dc06f750ce08be3e
Billy-Xu <billy.xu@xplusz.com>
2019-09-04 23:43:11 +0800

    parser v0.02, add error tracking

--- parser/parser.go
+++ parser/parser.go
@@ -1,26 +1,41 @@
 package parser
 
 import (
+	"fmt"
+
 	"billy.xu.code/waiig/ast"
 	"billy.xu.code/waiig/lexer"
 	"billy.xu.code/waiig/token"
 )
 
 type Parser struct {
-	l *lexer.Lexer
+	l      *lexer.Lexer
+	errors []string
 
 	curToken  token.Token
 	peekToken token.Token
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	// Read two tokens, so curToken and peekToken are ready
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
@@ -86,6 +101,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -18,6 +18,7 @@ func TestLetStatements(t *testing.T) {
 	p := New(l)
 
 	program := p.ParseProgram()
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -40,6 +41,23 @@ func TestLetStatements(t *testing.T) {
 		}
 	}
 }
+
+func TestLetStatementsWithError(t *testing.T) {
+	input := `
+		let x 5;
+		let = 10;
+		let 838383;
+	`
+
+	l := lexer.New(input)
+	p := New(l)
+
+	p.ParseProgram()
+	if 3 != len(p.Errors()) {
+		t.Fatalf("error are not expected.")
+	}
+}
+
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	if s.TokenLiteral() != "let" {
 		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
@@ -62,4 +80,16 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	}
 
 	return true
+}
+
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
 }
